09afa427e2f5081ab07bc1936750d352dec281b7
my28lee <my28.lee@secui.com>
2016-10-14 15:44:34 +0900

    commit

--- app/memo/views.py
+++ app/memo/views.py
@@ -36,7 +36,6 @@ def streamed_response():
 
 ConfigParser = None
 
-
 @memo_blueprint.route('/history')
 def svn_path_select():
     global ConfigParser
@@ -50,7 +49,6 @@ def svn_path_select():
 
     return render_template('memo/history_list.html', data=svn_list, rooturl=svnurl)
 
-
 @memo_blueprint.route('/history_add', methods=['POST'])
 def svn_path_add():
     query = 'INSERT INTO svn_info(product,s_path_url,s_start_revision,s_last_revision) values(?,?,?,?);'
@@ -59,24 +57,13 @@ def svn_path_add():
     g.db.commit()
     return redirect(url_for('.svn_path_select'))
 
-
 @memo_blueprint.route('/history_del/<int:id>')
 def svn_path_delete(id):
-    global ConfigParser
-    if ConfigParser is None:
-        import ConfigParser
-    config = ConfigParser.ConfigParser();
-    config.read('local_history.ini')
-    svnurl = config.get('svn', 'rooturl')
-    svn_list = g.db.execute('select * from svn_history where s_path_id=' + str(id)).fetchall()
-    for row in svn_list:
-        g.db.execute('delete from svn_history_file where id=' + str(row[0]))
-
+    g.db.execute('delete from svn_history_file where svn_id in (select id from svn_history where s_path_id='+str(id)+')')
     g.db.execute('delete from svn_history where s_path_id=' + str(id))
     g.db.execute('delete from svn_info where s_path_id=' + str(id))
     g.db.commit()
     return redirect(url_for('.svn_path_select'))
-
 
 @memo_blueprint.route('/history/info', methods=['GET', 'POST'])
 def svn_history():
@@ -103,7 +90,6 @@ def svn_history():
         svnrooturl = config.get('svn', 'rootmfiurl')
         svnurl = svnrooturl + path_info[0][1]
 
-    print pSID, path_info[0][1], path_info[0][2], path_info[0][3]
     s = svncheck(svnrooturl, path_info[0][1], '', '', config.get('svn', 'uid'), config.get('svn', 'upass'))
 
     # 마지막 리비전 조회
@@ -111,7 +97,6 @@ def svn_history():
 
     resultlist = []
 
-    print 'last_revision', path_info[0][3], last_revision
     # DB에 저장된 최종 리비전이 리파지토리 최종 리비전 보다 작을 경우 변경 로그 조회
     if int(path_info[0][3]) < last_revision:
         cur = g.db.cursor()
@@ -160,16 +145,12 @@ def svn_history():
     else:
         pOffset = 0
 
-    tmpQuery = ' order by s_revision desc limit ' + str(pOffset) + ',10'
-
     if pSvnId:
-        query = 'select * from svn_history where s_path_id=? and s_id LIKE ?' + tmpQuery
-        cur = g.db.execute(query, [path_info[0][0], '%' + pSvnId + '%'])
+        query = 'select * from svn_history where s_path_id=? and s_id LIKE ? order by s_revision desc limit ?,10'
+        cur = g.db.execute(query, [path_info[0][0], '%' + pSvnId + '%',pOffset])
     else:
-        query = 'select * from svn_history where s_path_id=?' + tmpQuery
-        cur = g.db.execute(query, [path_info[0][0]])
-
-    print query
+        query = 'select * from svn_history where s_path_id=?  order by s_revision desc limit ?,10'
+        cur = g.db.execute(query, [path_info[0][0],pOffset])
 
     for row in cur.fetchall():
         subquery = 'select * from svn_history_file where svn_id=?'
